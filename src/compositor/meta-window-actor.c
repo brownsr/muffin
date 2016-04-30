4946c955ffebfd1d9ff69e3fa0159ff9f55b9f51
brownsr <simon@greatgiftsandtoys.co.uk>
2016-04-30 20:56:13 +0100

    support _NET_WM_OPAQUE_REGION https://github.com/GNOME/mutter/commit/a613a55658c5717481e1f2d34987715d5f0a5ca4

--- src/compositor/meta-window-actor.c
+++ src/compositor/meta-window-actor.c
@@ -75,6 +75,9 @@ struct _MetaWindowActorPrivate
 
   /* A region that matches the shape of the window, including frame bounds */
   cairo_region_t   *shape_region;
+  /* The opaque region, from _NET_WM_OPAQUE_REGION, intersected with
+   * the shape region. */
+  cairo_region_t   *opaque_region;
   /* A rectangular region with the visible extents of the window */
   cairo_region_t   *bounding_region;
   /* The region we should clip to when painting the shadow */
@@ -471,6 +474,7 @@ meta_window_actor_dispose (GObject *object)
   meta_window_actor_detach (self);
 
   g_clear_pointer (&priv->shape_region, cairo_region_destroy);
+  g_clear_pointer (&priv->opaque_region, cairo_region_destroy);
   g_clear_pointer (&priv->bounding_region, cairo_region_destroy);
   g_clear_pointer (&priv->shadow_clip, cairo_region_destroy);
 
@@ -1765,8 +1769,8 @@ meta_window_actor_get_obscured_region (MetaWindowActor *self)
 {
   MetaWindowActorPrivate *priv = self->priv;
 
-  if (!priv->argb32 && priv->opacity == 0xff && priv->back_pixmap)
-    return priv->shape_region;
+  if (priv->back_pixmap && priv->opacity == 0xff)
+    return priv->opaque_region;
   else
     return NULL;
 }
@@ -2342,38 +2346,59 @@ check_needs_reshape (MetaWindowActor *self)
 
     needs_mask = (region != NULL) || (priv->window->frame != NULL);
 
-    if (region == NULL)
+   if (region != NULL)
+     {
+       /* The shape we get back from the client may have coordinates
+        * outside of the frame. The X SHAPE Extension requires that
+        * the overall shape the client provides never exceeds the
+        * "bounding rectangle" of the window -- the shape that the
+        * window would have gotten if it was unshaped. In our case,
+        * this is simply the client area.
+        */
+       cairo_region_intersect_rectangle (region, &client_area);
+     }
+   else
+     {
+       /* If we don't have a shape on the server, that means that
+        * we have an implicit shape of one rectangle covering the
+        * entire window. */
+       region = cairo_region_create_rectangle (&client_area);
+     }
+ 
+    /* The region at this point should be constrained to the
+     * bounds of the client rectangle. */
+  
+   if (priv->argb32 && priv->window->opaque_region != NULL)
     {
-      /* If we don't have a shape on the server, that means that
-       * we have an implicit shape of one rectangle covering the
-       * entire window. */
-      region = cairo_region_create_rectangle (&client_area);
-    }
-
-    if (needs_mask) 
-    {
-      /* This takes the region, generates a mask using GTK+
-       * and scans the mask looking for all opaque pixels,
-       * adding it to region.
+      /* The opaque region is defined to be a part of the
+       * window which ARGB32 will always paint with opaque
+       * pixels. For these regions, we want to avoid painting
+       * windows and shadows beneath them.
+       *
+       * If the client gives bad coordinates where it does not
+       * fully paint, the behavior is defined by the specification
+       * to be undefined, and considered a client bug. In mutter's
+       * case, graphical glitches will occur.
        */
-      build_and_scan_frame_mask (self, &borders, &client_area, region);
+      priv->opaque_region = cairo_region_copy (priv->window->opaque_region);
+      cairo_region_translate (priv->opaque_region, client_area.x, client_area.y);
+      cairo_region_intersect (priv->opaque_region, region);
     }
+  else if (priv->argb32)
+    priv->opaque_region = NULL;
+  else
+    priv->opaque_region = cairo_region_reference (region);
 
-    priv->shape_region = region;
-
-   /* Our "shape_region" is called the "bounding region" in the X Shape
-    * Extension Documentation.
-    *
-    * Our "bounding_region" is called the "bounding rectangle", which defines
-    * the shape of the window as if it the window was unshaped.
-    *
-    * The X Shape extension requires that the "bounding region" can never
-    * extend outside the "bounding rectangle", and says it must be implicitly
-    * clipped before rendering. The region we get back hasn't been clipped.
-    * We explicitly clip the region here.
-    */
-   if (priv->bounding_region != NULL)
-     cairo_region_intersect (priv->shape_region, priv->bounding_region);
+    if (needs_mask)
+      {
+        /* This takes the region, generates a mask using GTK+
+        * and scans the mask looking for all opaque pixels,
+        * adding it to region.
+        */
+       build_and_scan_frame_mask (self, &borders, &client_area, region);
+     }
+ 
+  priv->shape_region = region;
 
   priv->needs_reshape = FALSE;
   meta_window_actor_invalidate_shadow (self);
